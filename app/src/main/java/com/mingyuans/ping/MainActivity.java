305f9f9f45d77413da7999fd8371a939ea99721a
yanxq <yanxiangqunwork@163.com>
2017-10-24 11:46:21 +0800

    [需求] 提交 demo

--- app/src/main/java/com/mingyuans/ping/MainActivity.java
+++ app/src/main/java/com/mingyuans/ping/MainActivity.java
@@ -2,6 +2,9 @@ package com.mingyuans.ping;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button clickButton = (Button) findViewById(R.id.tv_main_click);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                while (true) {
+                    tryPing("www.taobao.com");
+                }
+            }
+        });
     }
+
+
+    private void tryPing(String hostname) {
+        String pingCommand = AndroidPing.createSimplePingCommand(hostname,1,1);
+        Log.i("AndroidPing","Ping command: " + pingCommand);
+        String pingAnswerString = AndroidPing.ping(pingCommand);
+        Log.i("AndroidPing","Ping answer string: " + pingAnswerString);
+        AndroidPing.PingAnswer pingAnswer = AndroidPing.parsePingAnswerString(pingAnswerString);
+        if (pingAnswer != null) {
+            Log.i("AndroidPing",String.valueOf(pingAnswer));
+        } else {
+            Log.i("AndroidPing","ping answer is null!!!");
+        }
+    }
+
 }
